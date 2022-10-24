d22d5ac4fa553f716651e30237eb8da09878d56e
Eli Olmos <eliolmos81@gmail.com>
2022-10-24 09:38:56 -0300

    le doy funcionalidad a mi codigo para completar la gestión de reserva

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 let savedDNI = "28656220";
+let dia,mes,anio,hora=0;
 
 function login() {
   let ingresar = false;
@@ -30,72 +31,82 @@ if (login()) {
         alert(
           "A continuación le solicitaremos los datos para gestionar su reserva"
         );
-        let dia = parseInt(prompt("Ingrese el día de su reserva dd"));
-        let mes = parseInt(prompt("Ingrese el mes de su reserva mm"));
-        let anio = parseInt(prompt("Ingrese el año de su reserva aaaa"));
-        let hora = parseInt(prompt("Ingrese la hora de su reserva hh"));
+        dia = parseInt(prompt("Ingrese el día de su reserva dd"));
+        mes = parseInt(prompt("Ingrese el mes de su reserva mm"));
+        anio = parseInt(prompt("Ingrese el año de su reserva aaaa"));
+        hora = parseInt(prompt("Ingrese la hora de su reserva hh"));
         alert(
           "Su reserva se ha realizado para el día " +
             reservar(dia, mes, anio, hora)
         );
 
         break;
-      case "2":{
-        let nvaReserva = prompt(
-          "Indica los datos a modificar de tu reserva \n1 - Día \n2 - Mes \n3 - Año \n4 - Hora"
-        );
-       if(nvaReserva==1){
-            let nvoDia = parseInt(
+      case "2":
+        {
+          let nvaReserva = prompt(
+            "Indica los datos a modificar de tu reserva \n1 - Día \n2 - Mes \n3 - Año \n4 - Hora"
+          );
+          if (nvaReserva == 1) {
+            dia = parseInt(
               prompt("Ingrese el nuevo día de su reserva (dd)")
             );
             alert(
               "Se ha cambiado exitosamente el día de su reserva para el " +
-                nvoDia);
-            }
-            else if(nvaReserva==2){
-            nvoMes = parseInt(
+                reservar(dia,mes,anio,hora)
+            );
+          } else if (nvaReserva == 2) {
+            mes = parseInt(
               prompt("Ingrese el nuevo mes de su reserva (mm)")
             );
             alert(
-              "Se ha cambiado exitosamente el mes de su reserva para el " +
-                nvoMes
+              "Se ha cambiado exitosamente el día de su reserva para el " +
+                reservar(dia,mes,anio,hora)
             );
-            }
-            else if(nvaReserva==3){
-            nvoAnio = parseInt(
+          } else if (nvaReserva == 3) {
+            anio = parseInt(
               prompt("Ingrese el nuevo año de su reserva (aaaa)")
             );
             alert(
-              "Se ha cambiado exitosamente el año de su reserva para el " +
-                nvoAnio
+              "Se ha cambiado exitosamente el día de su reserva para el " +
+                reservar(dia,mes,anio,hora)
             );
-            }
-            else if(nvaReserva==4){
-            nvaHora = parseInt(
+          } else if (nvaReserva == 4) {
+            hora = parseInt(
               prompt("Ingrese la nueva hora de su reserva (hh)")
             );
-            alert("Se ha cambiado exitosamente la hora de su reserva para las " + nvaHora +"hs.");
-            }
-          else{
+            alert(
+              "Se ha cambiado exitosamente el día de su reserva para el " +
+                reservar(dia,mes,anio,hora)
+            );
+          } else {
             alert("Elegiste una opción inválida");
+          }
         }
-      }
-        break;      
+        break;
       case "3":
+        if (dia != 0 && mes != 0 && anio !=0 && hora !=0 ){
         alert(
-          "Vamos a proceder a la cancelación de su cancha asignada para el usuario con DNI: " +
-            savedDNI
+          "Vamos a proceder a la cancelación de su reserva de cancha asignada para el usuario con DNI: " +
+            savedDNI +" del día " + reservar(dia,mes,anio,hora)
         );
+
         let cancela = prompt(
           "¿Estás seguro que desea cancelar su reserva\n 1- Si \n 2- No"
         );
         if (cancela == "1") {
+          dia,mes,anio,hora=0;
           alert("Se ha cancelado su reserva exitosamente");
         } else if (cancela == 2) {
           alert("Se mantendrá su reserva actual");
         } else {
           alert(
             "Usted ha ingresado una opción incorrecta, le solicitamos reintente con el menú principal"
+          );
+          }
+        }
+        else{
+          alert(
+            "Usted no tiene reservas para cancelar lo enviaremos al menú principal"
           );
         }
         break;
@@ -114,15 +125,6 @@ if (login()) {
 }
 
 function reservar(dia, mes, anio, hora) {
-  let reserva =
-    "su reserva se registró para el día " +
-    dia +
-    "/" +
-    mes +
-    "/" +
-    anio +
-    " a las " +
-    hora +
-    " hs";
+  let reserva = dia +"/" + mes +"/"+anio+" a las " +hora +" hs";
   return reserva;
 }
